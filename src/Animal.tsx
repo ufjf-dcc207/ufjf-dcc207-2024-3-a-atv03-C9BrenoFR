52783d1ca69db9e0868b7a32483811cadba2fe15
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2024-12-02 20:49:45 -0300

    Adiciona mais animais

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -4,14 +4,14 @@ interface AnimalProps{
     icone: string;
     nome: string;
     peso: number;
-    extincao: boolean;
+    extincao?: boolean;
 }
 
 export default function Animal({icone, nome, peso, extincao}: AnimalProps){
     return <div className="animal">
         <div className="icone">{icone}</div>
         <div className="nome">{nome}</div>
-        {peso > 0 ?
+        {peso > 0.0 ?
          <div className="peso">{peso.toLocaleString()} Kg</div> : 
          <div className="peso">Peso Desconhecido</div>
         }
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <div className="app">
-      <Animal icone='🦁' nome='Leão' peso={190.37} extincao={false} />
+      <Animal icone='🦁' nome='Leão' peso={190.37} extincao />
+      <Animal icone='🦒' nome='Girafa' peso={1200} extincao />
+      <Animal icone='🦜' nome='Papagaio' peso={0.120} extincao={false} />
     </div>
   )
 }
