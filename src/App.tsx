c2cb6ece95c92c9337b73d566833c99413326331
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2024-12-05 22:12:39 -0300

    Coloca Animal dentro de exibição

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,15 @@ function App() {
       <Exibicao inicio={new Date("2024-11-06T08:00:00.000-03:00")} 
       fim={new Date("2024-11-06T16:00:00.000-03:00")}
       local='A1'
-      />
+      >
       <Animal icone='🦁' nome='Leão' peso={190.37} extincao />
       <Animal icone='🦒' nome='Girafa' peso={1200} extincao />
+      </Exibicao>
       <Exibicao inicio={new Date("2024-11-06T08:00:00.000-03:00")} 
       fim={new Date("2024-11-06T16:00:00.000-03:00")}
-      local='B1'/>
+      local='B1'>
       <Animal icone='🦜' nome='Papagaio' peso={0.120} extincao={false} />
+      </Exibicao>
     </div>
   )
 }
--- src/exibicao.tsx
+++ src/exibicao.tsx
@@ -4,14 +4,18 @@ interface exibicaoProps{
     inicio: Date,
     fim: Date,
     local: string
+    children: React.ReactNode
 }
 
-export default function Exibicao({inicio, fim, local}: exibicaoProps){
+export default function Exibicao({inicio, fim, local, children}: exibicaoProps){
     return (
     <div className="exibicao">
         <div className="inicio">Abertura: {inicio.toLocaleString()}</div>
         <div className="fim">Fechamento: {fim.toLocaleString()}</div>
         <div className="local">Cercado: {local}</div>
+        <div className="animais">
+            {children}
+        </div>
     </div>
 )
 }
